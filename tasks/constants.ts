cab017d73ffad9982775af39f1fb62d5e944fa15
scorpion9979 <ahmed.i.tawfeeq@protonmail.com>
2022-03-27 10:14:32 +0300

    feat(tasks): add VaultFactory deploy task feat(tasks): add subtasks

---
+++ tasks/constants.ts
@@ -0,0 +1,3 @@
+export const TASK_DEPLOY_CONTRACT_VAULT_FACTORY: string = "deploy:contract:vault-factory";
+export const SUBTASK_DEPLOY_WAIT_FOR_CONFIRMATIONS: string = "deploy:wait-for-confirmations";
+export const SUBTASK_VERIFY_VERIFY: string = "verify:verify";
